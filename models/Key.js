cc04871e3daa9839172e99800141f6f1a08c4bf1
vaniaggarwal <52869093+vaniaggarwal@users.noreply.github.com>
2025-02-02 22:21:35 -0800

    Database
    trying to connect to database

--- models/Key.js
+++ models/Key.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose'); 
+// call over to hashutils.js
 
 // keys 
 const KeySchema = new mongoose.Schema({
     _id: 
-        { type: String, required: true }, // unique key ID
+        { type: String, required: true }, // unique key ID aka hashed key
+    originalKey: 
+        { type: String, required: true }, // original key before hashing, can be deleted later but keeping for testing purposes
     platform: 
         { type: String, required: true }, // social media platform name
     publicKey: 
