4be0ab27f68401b21f103b8b52726682f53dc236
Mia <mia.lassiter14@gmail.com>
2025-02-20 17:45:33 -0800

    install selenium-webdriver for plugin functionality
    package for automating browser interaction from Python

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+
 const { spawn } = require("child_process");
 
 const pythonScriptPath = "scraper/LI/scrape_lipost.py";
@@ -9,3 +10,37 @@ const child = spawn("python3", [pythonScriptPath], {
 child.on("exit", (code)=>{
     console.log(`Python script exited with code ${code}`);
 });
+// const app = express();
+
+// const pythonPromise = () => {
+//     return new Promise ((resolve, reject) => {
+//         const pythonScriptPath = spawn("python", ["scraper/url.py"]);
+//         pythonPromise.stdout.on("data", (data) => {
+//             resolve(data.toString());
+//         });
+
+//         python.stderr.on("data", (data) => {
+//             reject(data.toString());
+//         });
+//     });
+// };
+
+// app.get("/:domain", async(requestAnimationFrame, res) => {
+//     const domainName = await pythonPromise();
+//     res.send(domainName + req.params.name);
+// });
+
+// const pythonScriptPath = "scraper/url.py";
+// const child = spawn("python3", [pythonScriptPath], {
+//     stdio: "inherit",
+// });
+
+
+// child.on("exit", (code)=>{
+//     console.log(`Python script exited with code ${code}`);
+// });
+
+// const pythonScriptPath = "scraper/LI/scrape_lipost.py";
+// const child = spawn("python3", [pythonScriptPath], {
+//     stdio: "inherit",
+// }); //spawn py script and use same input/output as Node.js process
