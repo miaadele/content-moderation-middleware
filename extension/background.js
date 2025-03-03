212e73738c31e82ff9c3ea6fbacedd411df634eb
Mia <mia.lassiter14@gmail.com>
2025-03-02 22:09:25 -0800

    Attempting to retrieve unique post URLs, not working yet
    LinkedIn uses the class "artdeco-toast-item__cta" for the specific post URLs
    
    However, I can't seem to retrieve elements with that class name
    
    Tested funcitonality by retrieving elements by tag name
    
    That worked...

--- extension/background.js
+++ extension/background.js
@@ -6,7 +6,22 @@ chrome.contextMenus.create({
     contexts: ['all'] // change later to click on a post
 })
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-    console.log(info)
-    console.log(tab)
-})
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Background.js is running');
+});
+
+// chrome.action.onClicked.addListener((tab) => {
+//     console.log('Extension clicked');
+
+//     chrome.scripting.executeScript({
+//         target: {tabId: tab.id},
+//         files: ['content.js']
+//     });
+// });
+
+// //listen for message from content.js
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//     if (message.type === 'links') {
+//         console.log('Received links: ', message.data);
+//     }
+// })
--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,10 @@
 console.log("Content script running!"); 
+
+//const links = document.getElementsByClassName("artdeco-toast-item__cta"); //links is an HTMLCollection
+const links = document.getElementsByTagName("a");
+const linksArray = Array.from(links).map(link => link.outerHTML); //convert links to linksArray
+console.log(links);
+// chrome.runtime.sendMessage({
+//     type: 'elements',
+//     data: linksArray
+// });
