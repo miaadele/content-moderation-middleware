dd34f7a87a060c4d404059f705afc7f3cd77f11f
Mia <mia.lassiter14@gmail.com>
2025-03-04 16:34:41 -0800

    Browser UI: Child menu
    Add child menu to context menu
    Change placeholder image
    
    have to add function to change activation based on where the user clicks (i.e. context tag in context menu item). This would only be valid for LinkedIn posts, though...eventually need to have plugin recognize domain name of URL and call python scripts accordingly

--- extension/background.js
+++ extension/background.js
@@ -1,10 +1,44 @@
 //Service worker
 
+
+//Context menu creation
 chrome.contextMenus.create({
     id: 'ContextMenu',
     title: 'Ascertion',
+    type: "normal",
     contexts: ['all'] // change later to click on a post
-})
+});
+chrome.contextMenus.create({
+    id: 'ca',
+    title: 'Content Authorities',
+    parentId: "ContextMenu",
+    type: "normal"
+});
+chrome.contextMenus.create({
+    id: 'verify',
+    title: 'Verify Post',
+    parentId: "ContextMenu",
+    type: "normal"
+});
+chrome.contextMenus.create({
+    id: 'metadata',
+    title: 'View Metadata',
+    parentId: "ContextMenu",
+    type: "normal"
+});
+chrome.contextMenus.create({
+    id: 'radio1',
+    title: 'CA Option 1',
+    type: "radio",
+    parentId: "ca"
+});
+chrome.contextMenus.create({
+    id: 'radio2',
+    title: 'CA Option 2',
+    type: "radio",
+    parentId: "ca"
+});
+
 
 chrome.runtime.onInstalled.addListener(() => {
     console.log('Background.js is running');
