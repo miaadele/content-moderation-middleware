728022e01f9640ff171641e4b7d81d4369ed8989
Mia <mia.lassiter14@gmail.com>
2025-03-07 22:22:56 -0800

    Progress: Post detection
    Posts are detected and NodeList is returned
    Right-clicks are detected on post
    
    Background messages are not being received from the content script

--- extension/background.js
+++ extension/background.js
@@ -1,77 +1,65 @@
 //Service worker
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Content script running!");
-    const LIpost = document.querySelectorAll('.fie-impression-container');
-    // console.log(postElts);
-
-    LIpost.forEach( post => {
-        const postElts = post.querySelectorAll('*');
-        console.log(postElts);
-
-        postElts.forEach(element => {
-            element.addEventListener('click', function() {
-                console.log('Element clicked');
-            });
-        });
-    });
-})
-
-//Context menu creation
-chrome.contextMenus.create({
-    id: 'ContextMenu',
-    title: 'Ascertion',
-    type: "normal",
-    contexts: ['all'] // change later to click on a post
-});
-chrome.contextMenus.create({
-    id: 'ca',
-    title: 'Content Authorities',
-    parentId: "ContextMenu",
-    type: "normal"
-});
-chrome.contextMenus.create({
-    id: 'verify',
-    title: 'Verify Post',
-    parentId: "ContextMenu",
-    type: "normal"
-});
-chrome.contextMenus.create({
-    id: 'metadata',
-    title: 'View Metadata',
-    parentId: "ContextMenu",
-    type: "normal"
-});
-chrome.contextMenus.create({
-    id: 'radio1',
-    title: 'CA Option 1',
-    type: "radio",
-    parentId: "ca"
-});
-chrome.contextMenus.create({
-    id: 'radio2',
-    title: 'CA Option 2',
-    type: "radio",
-    parentId: "ca"
-});
-
-
 chrome.runtime.onInstalled.addListener(() => {
-    console.log('Background.js is running');
+    chrome.action.setBadgeText({
+        text: "OFF",
+    });
+    //Context menu creation
+    chrome.contextMenus.create({
+        id: 'ContextMenu',
+        title: 'Ascertion',
+        type: "normal",
+        contexts: ['all'], // change later to click on a post
+        visible: false //initially hidden
+    });
+    chrome.contextMenus.create({
+        id: 'ca',
+        title: 'Content Authorities',
+        parentId: "ContextMenu",
+        type: "normal"
+    });
+    chrome.contextMenus.create({
+        id: 'verify',
+        title: 'Verify Post',
+        parentId: "ContextMenu",
+        type: "normal"
+    });
+    chrome.contextMenus.create({
+        id: 'metadata',
+        title: 'View Metadata',
+        parentId: "ContextMenu",
+        type: "normal"
+    });
+    chrome.contextMenus.create({
+        id: 'radio1',
+        title: 'CA Option 1',
+        type: "radio",
+        parentId: "ca"
+    });
+    chrome.contextMenus.create({
+        id: 'radio2',
+        title: 'CA Option 2',
+        type: "radio",
+        parentId: "ca"
+    });
+});
+    
+
+//handle contextMenu clicks
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if(info.menuItemId === 'verify') {
+        console.log('Verify post');
+    }
+    else if(info.menuItemId === 'metadata') {
+        console.log('View metadata');
+    }
 });
 
-// chrome.action.onClicked.addListener((tab) => {
-//     console.log('Extension clicked');
 
-//     chrome.scripting.executeScript({
-//         target: {tabId: tab.id},
-//         files: ['content.js']
-//     });
-// });
-
-// //listen for message from content.js
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//     if (message.type === 'links') {
-//         console.log('Received links: ', message.data);
-//     }
-// })
+//listen for messages from content script to show the context menu
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    console.log('Received message in background: ', message);
+    if(message.action === 'showContextMenu') {
+        chrome.contextMenus.update('ContextMenu', {visible: true});
+    }
+});
