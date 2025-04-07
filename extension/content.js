51a3efda8f6bb593c7e9c82a34e78df101c42988
vaniaggarwal <52869093+vaniaggarwal@users.noreply.github.com>
2025-04-07 11:52:06 -0700

    Revert "Merge branch 'js_fns_scraping' into crypto"
    This reverts commit 9cac1a9d366d56d0690edcfd8cb8a80850b80052, reversing
    changes made to 3f9840d8c80bd6b23a85438ebe8bc08683ad14d1.

--- extension/content.js
+++ extension/content.js
@@ -1,19 +1,50 @@
 console.log("First line of content.js"); 
+/*
+function postData(input) {
+    $.ajax({
+        type: "POST",
+       // url: "../scraper/LI/scrape_lipost.py",
+        url: "http://localhost:8080/run-python", // sending to express server
+        contentType: 'application/json', 
+        data: JSON.stringify({
+            username: "username", 
+            password: "password",
+            postUrl: input
+        }),
+      //  data: { param: input }, 
+        //success: callbackfn
+        success: function(response) {
+            console.log("AJAX Success: ", response);
+        },
+        error: function(xhr, status, error) {
+            console.log("AJAX Error: ", status, error);
+        }
+    });
+}
+*/
 
+function callbackfn(response) {
+    console.log(response);
+}
+
+postData('data');
+
+// Use MutationObserver to detect when new posts are added dynamically
 const observer1 = new MutationObserver(() => {
     var posts = document.querySelectorAll('.fie-impression-container'); // LinkedIn post container
-    // Use MutationObserver to detect when new posts are added dynamically
-    posts.forEach(post => {
-        post.addEventListener('contextmenu', function() {
-        console.log('Right-click detected on post');
-        chrome.runtime.sendMessage({ action: 'showContextMenu' });
-        });
-    });
+    
+    // posts.forEach(post => {
+    //     post.addEventListener('contextmenu', function() {
+    //     console.log('Right-click detected on post');
+    //     chrome.runtime.sendMessage({ action: 'showContextMenu' }); // Send message to background script to show context menu
+    //     });
+    // });
 });
     
 observer1.observe(document.body, { childList: true, subtree: true });
 
 //Use MutationObserver listens for dynamic changes to the DOM
+/*
 const observer2 = new MutationObserver(() => {
     const links = document.getElementsByClassName("artdeco-toast-item__cta"); //links is an HTMLCollection
     console.log(links);
@@ -25,4 +56,5 @@ const observer2 = new MutationObserver(() => {
     });
 });
 
-observer2.observe(document.body, { childList: true, subtree: true });
+observer2.observe(document.body, { childList: true, subtree: true }); 
+*/
